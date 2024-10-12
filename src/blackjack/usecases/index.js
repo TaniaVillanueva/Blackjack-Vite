bd9dbdc956b159a64c14ffbf6d340c5177e28fee
Tania Villanueva <trodvil112@gmail.com>
2024-10-12 17:33:02 -0600

    Fin de refactorización

--- src/blackjack/usecases/index.js
+++ src/blackjack/usecases/index.js
@@ -1,3 +1,5 @@
 export {crearDeck} from './crear-deck';
 export {pedirCarta} from './pedir-carta';
 export { valorCarta } from './valor-carta';
+export { turnoComputadora } from './turno-computadora'
+export {crearCartaHTML} from './crear-carta-html';
